d59a9c5636fa661e2396877698f72aa89b1dda7f
kuangwj1210 <kuangwj1210@gmail.com>
2022-01-10 19:31:57 -0500

    created nav-bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,33 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TopNavComponent } from './top-nav/top-nav.component';
+import { HomeComponent } from './top-nav/home/home.component';
+import { AboutComponent } from './top-nav/about/about.component';
+import { BlogComponent } from './top-nav/blog/blog.component';
+import { PagesComponent } from './top-nav/pages/pages.component';
+import { ContactComponent } from './top-nav/contact/contact.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { RegisterComponent } from './register/register.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TopNavUserComponent } from './top-nav-user/top-nav-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavComponent,
+    HomeComponent,
+    AboutComponent,
+    BlogComponent,
+    PagesComponent,
+    ContactComponent,
+    SignInComponent,
+    RegisterComponent,
+    TopNavUserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
